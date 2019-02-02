96f7de517dc59218128ded169de298accb27b312
EmilFrom <emilfrom@gmail.com>
2019-02-02 19:01:10 +0100

    Update missiongothic.rb

--- missiongothic.rb
+++ missiongothic.rb
@@ -6,13 +6,13 @@ cask 'missiongothic' do
     name 'Mission Gothic'
     homepage 'http://www.losttype.com/font/?name=missiongothic'
 
-    font 'Mission Gothic Black Italic.otf'
-    font 'Mission Gothic Black.otf'
-    font 'Mission Gothic Bold Italic.otf'
-    font 'Mission Gothic Light Italic.otf'
-    font 'Mission Gothic Light.otf'
-    font 'Mission Gothic Regular Italic.otf'
-    font 'Mission Gothic Regular.otf'
-    font 'Mission Gothic Thin.otf'
-    font 'Mission Gothic Thin Italic.otf'
+    font "Mission Gothic Black Italic.otf"
+    font "Mission Gothic Black.otf"
+    font "Mission Gothic Bold Italic.otf"
+    font "Mission Gothic Light Italic.otf"
+    font "Mission Gothic Light.otf"
+    font "Mission Gothic Regular Italic.otf"
+    font "Mission Gothic Regular.otf"
+    font "Mission Gothic Thin.otf"
+    font "Mission Gothic Thin Italic.otf"
 end
