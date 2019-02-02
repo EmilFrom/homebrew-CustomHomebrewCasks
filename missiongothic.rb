b79b0adf85875aa2b37894abf9abb1508ff99d2b
EmilFrom <emilfrom@gmail.com>
2019-02-02 18:59:51 +0100

    Create missiongothic.rb

---
+++ missiongothic.rb
@@ -0,0 +1,18 @@
+cask 'missiongothic' do
+    version :latest
+    sha256 :no_check
+
+    url 'http://www.losttype.com/delivery/MissionGothic_Personal_License.zip'
+    name 'Mission Gothic'
+    homepage 'http://www.losttype.com/font/?name=missiongothic'
+
+    font 'Mission Gothic Black Italic.otf'
+    font 'Mission Gothic Black.otf'
+    font 'Mission Gothic Bold Italic.otf'
+    font 'Mission Gothic Light Italic.otf'
+    font 'Mission Gothic Light.otf'
+    font 'Mission Gothic Regular Italic.otf'
+    font 'Mission Gothic Regular.otf'
+    font 'Mission Gothic Thin.otf'
+    font 'Mission Gothic Thin Italic.otf'
+end
